96db5246bc944c61bda2d022ed5460670e259fa9
Devon <devonlumsden@mail.com>
2023-11-26 08:49:39 +1100

    refactor forloop into calculate func

--- pset1/credit/credit.c
+++ pset1/credit/credit.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void calculate(int maxamountcarddigits, int *sumoffirstnums, int *sumofsecondnums, long *cardnumber, int *cardlength);
+
 int main(void)
 {
     int maxamountcarddigits = 16;
-    long power = 10;
-    int oddnumber = 1;
-    int cardlength = 0;
+    int *cardlength = malloc(sizeof(int));
+    if (cardlength == NULL){
+        return 1;
+    }
+
     int *totalnumber = malloc(sizeof(int));
     if (totalnumber == NULL) {
         return 1;
@@ -28,42 +32,23 @@ int main(void)
     {
         return 1;
     }
+    
     printf("Number:");
     scanf("%ld", cardnumber);
 
     // Multiply every other digit by 2, starting with the number’s second-to-last digit, and then add those products’ digits together.
 
-    for (int place = 0; place < maxamountcarddigits; ++place)
-    {
-        long findnum = *cardnumber % power;
-        // printf("current: %ld oddnumber: %i cardnumber: %ld cardlength: %i \n", findnum, oddnumber, *cardnumber, cardlength);
-        if (oddnumber == 1) {
-            // *sumoffirstnums = *sumoffirstnums + *cardnumber;
-            // printf("firsts: %i \n", *sumoffirstnums);
-            *sumoffirstnums = *sumoffirstnums + findnum;
-            // printf("firsts: %i\n", *sumoffirstnums);
-            oddnumber = 2;
-        } else {
-            // *sumofsecondnums = *sumofsecondnums + (*cardnumber * 2);
-            // printf("seconds: %i \n", *sumofsecondnums);
-            *sumofsecondnums = *sumofsecondnums + (findnum * 2);
-            //printf("seconds: %i\n", *sumofsecondnums);
-            oddnumber = 1;
-        }
-        *cardnumber = *cardnumber / 10;
-        cardlength = cardlength + 1;
-        if (*cardnumber == 0){
-            break;
-        }
-    }
+    calculate(maxamountcarddigits, sumoffirstnums, sumofsecondnums, cardnumber, cardlength);
+
     //printf("seconds: %i firsts: %i \n", *sumoffirstnums, *sumofsecondnums);
     // Add the sum to the sum of the digits that weren’t multiplied by 2.
     *totalnumber = *sumoffirstnums + *sumofsecondnums;
+
     // If the total’s last digit is 0 (or, put more formally, if the total modulo 10 is congruent to 0), the number is valid!
     if (*totalnumber % 10 == 0) {
         //printf("Valid Card. Total number length: %i", cardlength);
         // American Express uses 15-digit numbers, MasterCard uses 16-digit numbers, and Visa uses 13- and 16-digit numbers.
-        switch(cardlength)
+        switch(*cardlength)
         {
             case 13:
                 printf("VISA\n");
@@ -84,4 +69,37 @@ int main(void)
     free(sumoffirstnums);
     free(sumofsecondnums);
     free(totalnumber);
+}
+
+void calculate(int maxamountcarddigits, int *sumoffirstnums, int *sumofsecondnums, long *cardnumber, int *cardlength) {
+    long power = 10;
+    int oddnumber = 1;
+
+    for (int place = 0; place < maxamountcarddigits; ++place)
+    {
+        long findnum = *cardnumber % power;
+        // printf("current: %ld oddnumber: %i cardnumber: %ld cardlength: %i \n", findnum, oddnumber, *cardnumber, cardlength);
+        if (oddnumber == 1)
+        {
+            // *sumoffirstnums = *sumoffirstnums + *cardnumber;
+            // printf("firsts: %i \n", *sumoffirstnums);
+            *sumoffirstnums = *sumoffirstnums + findnum;
+            // printf("firsts: %i\n", *sumoffirstnums);
+            oddnumber = 2;
+        }
+        else
+        {
+            // *sumofsecondnums = *sumofsecondnums + (*cardnumber * 2);
+            // printf("seconds: %i \n", *sumofsecondnums);
+            *sumofsecondnums = *sumofsecondnums + (findnum * 2);
+            // printf("seconds: %i\n", *sumofsecondnums);
+            oddnumber = 1;
+        }
+        *cardnumber = *cardnumber / 10;
+        *cardlength = *cardlength + 1;
+        if (*cardnumber == 0)
+        {
+                break;
+        }
+    }
 }
