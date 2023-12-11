622c50ae96b20ed3aff2e91d70fd244b07d300f9
Devon <devonlumsden@mail.com>
2023-12-11 22:01:31 +1100

    refactor: add decidewinner func

--- pset2/scrabble/scrabble.c
+++ pset2/scrabble/scrabble.c
@@ -4,13 +4,16 @@
 #include <string.h>
 
 int scoreword(char *word);
+void decidewinner(int player1score, int player2score);
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
-int main(void){
+int main(void)
+{
     int word_maxsize = 10;
     char *player1_word = malloc((word_maxsize * sizeof(char)) + 1);
     char *player2_word = malloc((word_maxsize * sizeof(char)) + 1);
-    if (player1_word == NULL || player2_word == NULL) {
+    if (player1_word == NULL || player2_word == NULL)
+    {
         return 1;
     }
 
@@ -19,27 +22,38 @@ int main(void){
     printf("Player 2: ");
     scanf("%s", player2_word);
 
-    int player1_score = scoreword(player1_word);
-    int player2_score = scoreword(player2_word);
-    
-    if (player1_score > player2_score){
-        printf("Player 1 wins!\n");
-    } else if (player1_score < player2_score){
-        printf("Player 2 wins!\n");
-    } else {
-        printf("Tie!\n");
-    }
+    decidewinner(scoreword(player1_word), scoreword(player2_word));    
 
     free(player1_word);
     free(player2_word);
     return 0;
 }
 
-int scoreword(char *word){
+int scoreword(char *word)
+{
     int wordscore = 0;
-    for (int placeinword = 0; word[placeinword] != '\0'; ++placeinword){
+    for (int placeinword = 0; word[placeinword] != '\0'; ++placeinword)
+    {
         char lettertocheck = toupper(word[placeinword]);
         wordscore = wordscore + POINTS[lettertocheck - 'A'];
     }
     return wordscore;
+}
+
+void decidewinner(int player1_score, int player2_score)
+{
+
+    if (player1_score > player2_score)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (player1_score < player2_score)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else
+    {
+        printf("Tie!\n");
+    }
+
 }
