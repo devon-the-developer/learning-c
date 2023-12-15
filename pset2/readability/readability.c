62ca0a381c8467126194a2793aa9e1f5b47b3f5f
Devon <devonlumsden@mail.com>
2023-12-15 23:09:21 +1100

    calculate wordcount in string

--- pset2/readability/readability.c
+++ pset2/readability/readability.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 #include <ctype.h>
 
-int countStringValues(char *text, int *letterCount, int *sentenceCount);
+int countStringValues(char *text, int *letterCount, int *wordCount, int *sentenceCount);
+// int getAverage(int numToAverage);
 
 int main(void)
 {
     int letterCount = 0;
+    int wordCount = 0;
     int sentenceCount = 0;
     char *grade1String = "One fish. Two fish. Red fish. Blue fish.";
     //printf("%s", grade1string);
-    countStringValues(grade1String, &letterCount, &sentenceCount);
-    printf("\n %i %i \n", letterCount, sentenceCount);
+    countStringValues(grade1String, &letterCount, &wordCount, &sentenceCount);
+    printf("\n %i %i %i \n", letterCount, wordCount, sentenceCount);
     return 0;
 }
 
-int countStringValues(char *text, int *letterCount, int *sentenceCount)
+int countStringValues(char *text, int *letterCount, int *wordCount, int *sentenceCount)
 {
    
     for (int currentIndex = 0; text[currentIndex] != '\0'; ++currentIndex)
@@ -26,9 +28,18 @@ int countStringValues(char *text, int *letterCount, int *sentenceCount)
         } else if (text[currentIndex] == '.')
         {
             ++*sentenceCount;
+            ++*wordCount;
+        } else if (text[currentIndex] == ' ' && currentIndex != 0 && text[currentIndex - 1] != '.')
+        {
+            ++*wordCount;
         }
     }
 
     return 0;
 
 }
+
+// int getAverage(int numToAverage)
+// {
+
+// }
