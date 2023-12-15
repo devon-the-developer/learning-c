42c25a6ef9a47551b8ebf624980e6a3664586e99
Devon <devonlumsden@mail.com>
2023-12-15 22:06:53 +1100

    add counttheletters() and print result

--- pset2/readability/readability.c
+++ pset2/readability/readability.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <ctype.h>
+
+int counttheletters(char *text);
 
 int main(void)
 {
-    printf("Hello project");
+    char *grade1string = "One fish. Two fish. Red fish. Blue fish.";
+    //printf("%s", grade1string);
+    int lettersinstring = counttheletters(grade1string);
+    printf("\n %i \n", lettersinstring);
     return 0;
 }
+
+int counttheletters(char *text)
+{
+    int count = 0;
+    for (int currentindex = 0; text[currentindex] != '\0'; ++currentindex)
+    {
+        printf("%c", text[currentindex]);
+        if(isalpha(text[currentindex]))
+        {
+            ++count;
+        }
+    }
+    return count;
+}
+
