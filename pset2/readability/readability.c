00134193e64820caaa0ebef41eb8abe3ddec718e
Devon <devonlumsden@mail.com>
2023-12-16 11:38:10 +1100

    alter text to print

--- pset2/readability/readability.c
+++ pset2/readability/readability.c
@@ -14,27 +14,27 @@ int main(void)
     //char *grade1String = "One fish. Two fish. Red fish. Blue fish.";
     //char *grade2String = "Would you like them here or there? I would not like them here or there. I would not like them anywhere.";
     //char *grade3String = "Congratulations! Today is your day. You're off to Great Places! You're off and away!";
-    char *stringToGrade = "Harry Potter was a highly unusual boy in many ways. For one thing, he hated the summer holidays more than any other time of year. For another, he really wanted to do his homework, but was forced to do it in secret, in the dead of the night. And he also happened to be a wizard.";
-        countStringValues(stringToGrade, &letterCount, &wordCount, &sentenceCount);
-    printf("\n %i %i %i \n", letterCount, wordCount, sentenceCount);
+    //char *grade5String = "Harry Potter was a highly unusual boy in many ways. For one thing, he hated the summer holidays more than any other time of year. For another, he really wanted to do his homework, but was forced to do it in secret, in the dead of the night. And he also happened to be a wizard.";
+    char *stringToGrade = "A large class of computational problems involve the determination of properties of graphs, digraphs, integers, arrays of integers, finite families of finite sets, boolean formulas and elements of other countable domains.";
+
+    countStringValues(stringToGrade, &letterCount, &wordCount, &sentenceCount);
+    //printf("\n %i %i %i \n", letterCount, wordCount, sentenceCount);
 
     // L is average number of letters per 100 words
     float L = getAveragePer100Words(letterCount, wordCount);
     // S is average number of sentences per 100 words
     float S = getAveragePer100Words(sentenceCount, wordCount);
-    printf("%.2f \n", L);
-    printf("%.2f \n", S);
-    printf("%f \n", calculateTextScore(L, S));
+
     gradeScore(calculateTextScore(L, S));
+
     return 0;
 }
 
 int countStringValues(char *text, int *letterCount, int *wordCount, int *sentenceCount)
 {
-   
+    printf("Text: %s \n", text);
     for (int currentIndex = 0; text[currentIndex] != '\0'; ++currentIndex)
     {
-        printf("%c", text[currentIndex]);
         if(isalpha(text[currentIndex]))
         {
             ++*letterCount;
@@ -65,10 +65,11 @@ float calculateTextScore(float L, float S)
 
 int gradeScore(float score)
 {
+    // converting float to int just removes decimal, this line gives logic to round to the nearest integer.
     int roundedScore = (int)(score < 0 ? (score - 0.5) : (score + 0.5));
     if (roundedScore < 1)
     {
-        printf("Grade 1 \n");
+        printf("Before Grade 1 \n");
     }
     else if (roundedScore < 16)
     {
