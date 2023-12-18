3225bea35689b9732e971a44b586d62bc85d09e0
Devon <devonlumsden@mail.com>
2023-12-18 23:41:55 +1100

    convert text into cipher and display

--- pset2/substitution/substitution.c
+++ pset2/substitution/substitution.c
@@ -5,12 +5,14 @@
 
 int verify_user_key(char *string);
 int get_text_from_user(char *string);
+int convert_text(char *key, char *plain_text, char *cipher_text);
 
 int main(int argc, char *argv[])
 {
     // Declaring user key variable
     char *user_key = argv[1];
     char plain_text[99];
+    char cipher_text[99];
 
     // Checking for the correct number of arguments
     if (argc != 2)
@@ -21,7 +23,9 @@ int main(int argc, char *argv[])
 
     verify_user_key(user_key);
     get_text_from_user(plain_text);
+    convert_text(user_key, plain_text, cipher_text);
 
+    printf("ciphertext: %s\n", cipher_text);
     return 0;
 }
 
@@ -33,7 +37,7 @@ int verify_user_key(char *string)
     // Checking the user key is of type string
     if (user_key != NULL && user_key[0] != '\0')
     {
-        printf("user_key is a string\n");
+        return 0;
     }
     else
     {
@@ -71,5 +75,36 @@ int get_text_from_user(char *string)
     char *plain_text_pointer = string;
     printf("plaintext: ");
     fgets(plain_text_pointer, 99, stdin);
+    return 0;
+}
+
+int convert_text(char *key, char *plain_text, char *cipher_text)
+{
+    int current_index = 0;
+
+    while(plain_text[current_index] != '\0')
+    {
+        char letter_to_convert = plain_text[current_index];
+
+        // Check if current letter is in alphabet
+        if (isalpha(letter_to_convert))
+        {
+            // Convert to upper case if necessary
+            if (islower(letter_to_convert))
+            {
+                letter_to_convert = toupper(letter_to_convert);
+            }
+            // Find the index of the letter in alphabet
+            int letter_index_in_key = letter_to_convert - 'A';
+            // Find this index in the key and add it to the cipher text
+            cipher_text[current_index] = key[letter_index_in_key];
+        } else {
+            // if not not in alphabet just leave as is.
+            cipher_text[current_index] = letter_to_convert;
+        }
+
+        current_index++;
+    }
+    
     return 0;
 }
