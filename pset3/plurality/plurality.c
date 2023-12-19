e3e23dec3c1f44a6baf72e5bc09bd162e43d42d4
Devon <devonlumsden@mail.com>
2023-12-19 12:12:01 +1100

    accept candidates from commandline args

---
+++ pset3/plurality/plurality.c
@@ -0,0 +1,50 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char *argv[])
+{
+    if(argc < 2) 
+    {
+        printf("Usage: %s <candidate1> <candidate2> <candidate3> ... \n", argv[0]);
+        return 1;
+    }
+
+    int num_candidates = argc - 1;
+
+    char **candidates = (char **)malloc(num_candidates * sizeof(char *));
+
+    // Checks if the pointer is Null, indicating an failure in memory allocation.
+    if (candidates == NULL)
+    {
+        printf("Memory allocation failed. \n");
+        return 1;
+    }
+
+    for(int current_index = 0; current_index < num_candidates; current_index++)
+    {
+        //copy argument to candidates 
+        candidates[current_index] = strdup(argv[current_index + 1]);
+
+        //check candidate isn't NULL --> failure in memory allocation
+        if(candidates[current_index] == NULL)
+        {
+            printf("Memory allocation failed for sttring %d. \n", current_index + 1);
+
+            //Clean up: Free previously allocated Memory
+            for (int index_to_clean = 0; index_to_clean < current_index; index_to_clean++)
+            {
+                free(candidates[index_to_clean]);
+            }
+
+            free(candidates);
+            return 1;
+        }
+
+        printf("Candidate %i: %s \n", current_index + 1, candidates[current_index]);
+    }
+
+    return 0;
+}
